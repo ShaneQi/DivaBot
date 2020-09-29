9466e8999aeb6b5cf277deb9bd39bb7dfa66ade4
Shane Qi <qizengtai@gmail.com>
2020-09-29 11:39:44 -0500

    Found back Arguments.

--- Package.swift
+++ Package.swift
@@ -12,14 +12,15 @@ let package = Package(
 			targets: ["DivaBot"]),
 		],
 	dependencies: [
-		.package(url: "https://github.com/shaneqi/ZEGBot.git", from: Version(4, 2, 6))
+		.package(url: "https://github.com/shaneqi/ZEGBot.git", from: Version(4, 2, 6)),
+		.package(url: "https://github.com/shaneqi/Arguments.git", from: Version(1, 0, 1))
 	],
 	targets: [
 		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
 		// Targets can depend on other targets in this package, and on products in packages which this package depends on.
 		.target(
 			name: "DivaBot",
-			dependencies: ["ZEGBot"]),
+			dependencies: ["ZEGBot", "Arguments"]),
 		.testTarget(
 			name: "DivaBotTests",
 			dependencies: ["DivaBot"]),
